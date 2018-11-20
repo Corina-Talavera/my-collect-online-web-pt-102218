b2a54349f7b4a7adc2744aa60133383352f99231
Corina Talavera <corina.talavera@gmail.com>
2018-11-20 20:19:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,50 +1,9 @@
-def my_collect
-  languages = ["ruby", "javascript", "python", "objective-c"]
-  students =["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
-
-def can_handle_an_empty_collection
-    empty_array = []
-    counter = 0
-      my_collect(empty_array) do |x|
-        counter += 1
-      end
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array.push(yield array[i])
+    i += 1
   end
-
-def     
-    my_collect(languages) do |language|
-      expect(language).to_not eq(nil)
-    end
-  end
-
-    my_collect(collection) do |lang|
-  lang.upcase
-end
-# => ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
-  end
-
-  it 'does not modify the original collection' do
-    my_collect(languages) do |language|
-      language.upcase
-    end
-    expect(languages).to eq(['ruby', 'javascript', 'python', 'objective-c'])
-  end
-
-  it "yields the correct element from the given collection, in this case students" do
-    my_collect(students) do |student|
-      expect(student).to_not eq(nil)
-    end
-  end
-
-  it "returns a new collection of appropriately modified elements, in this case student first names" do
-    expect(my_collect(students) do |student|
-      student.split(" ").first
-    end).to eq(["Tim", "Tom", "Sophie", "Antoin"])
-  end
-
-  it 'does not modify the original collection' do
-    my_collect(students) do |student|
-      student.split(" ").first
-    end
-    expect(students).to eq(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'])
-  end
+  new_array
 end
